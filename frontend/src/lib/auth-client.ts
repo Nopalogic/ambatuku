ac951c05e0ef8465f06ad352150523ebe5998e88
Nopalogic <naufaladhi02@gmail.com>
2025-12-04 23:59:58 +0700

    feat(auth): implement authentication using better-auth
    This commit integrates the `better-auth` library to manage the complete frontend authentication flow.

---
+++ frontend/src/lib/auth-client.ts
@@ -0,0 +1,18 @@
+import { createAuthClient } from "better-auth/react";
+
+import { LOCAL_STORAGE_BETTER_AUTH_TOKEN_KEY } from "@/features/auth/constants/local-storage";
+
+export const authClient = createAuthClient({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  fetchOptions: {
+    credentials: "omit",
+    auth: {
+      type: "Bearer",
+      token: async () => {
+        const token = localStorage.getItem(LOCAL_STORAGE_BETTER_AUTH_TOKEN_KEY);
+
+        return token ?? undefined;
+      },
+    },
+  },
+});
